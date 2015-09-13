3c06ae760e8363fe63769d57a07f2303b5658767
Ludovic Rousseau <ludovic.rousseau@free.fr>
2015-09-13 18:23:11 +0200

    Preload the next image
    The next image is used as the background image for the "next" button.
    When the image is changed the next image is already loaded by the
    browser.

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -423,7 +423,7 @@ view.photo = {
 			let nextPhotoID = album.json.content[photoID].nextPhoto
 			let nextPhoto   = album.json.content[nextPhotoID]
 
-			$nextArrow.css('background-image', lychee.html`linear-gradient(to bottom, rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)), url("$${ nextPhoto.thumbUrl }")`)
+			$nextArrow.css('background-image', lychee.html`linear-gradient(to bottom, rgba(0, 0, 0, .4), rgba(0, 0, 0, .4)), url("$${ nextPhoto.url }")`)
 
 		}
 
